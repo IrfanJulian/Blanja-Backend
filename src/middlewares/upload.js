552cab7cf76a62c911773e93da3288190ae4c3da
Ipan <irfanjulian23@gmail.com>
2022-11-07 11:28:37 +0700

    test

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -24,7 +24,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../../upload');
+    cb(null, './upload');
   },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
