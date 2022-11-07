fc223e95e79ab4726e0bb55b47d821632873c492
Ipan <irfanjulian23@gmail.com>
2022-11-07 11:39:47 +0700

    unused local storage

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -22,19 +22,19 @@ const multer = require('multer')
 
 //   module.exports = upload
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './upload');
-  },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname)
-    },
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, './upload');
+//   },
+//     filename: function (req, file, cb) {
+//       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+//       cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname)
+//     },
+// });
 
 const upload = multer({
   limits: { fileSize: 10 * Math.pow(1024, 2 /* MBs*/) },
-  storage: storage,
+  // storage: storage,
   fileFilter: (req, file, cb) => {
     if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg' || file.mimetype === 'image.jfif') {
       cb(null, true);
