9fe41f5e84a016b63d56803a4a0d6360d41cae2d
Ipan <irfanjulian23@gmail.com>
2022-11-12 14:47:38 +0700

    validation

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const categoryController = require('../controllers/category')
-const { admin } = require('../middlewares/auth')
+// const { admin } = require('../middlewares/auth')
 // const {hitCache} = require('../middlewares/redis')
 
 router.get('/', categoryController.get)
-router.post('/', admin, categoryController.insert)
-router.put('/:id', admin, categoryController.update)
-router.delete('/:id', admin, categoryController.deleteData)
+router.post('/', categoryController.insert)
+router.put('/:id', categoryController.update)
+router.delete('/:id', categoryController.deleteData)
 
 module.exports = router
